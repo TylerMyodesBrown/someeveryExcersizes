8b0d160efacd06193086650f13de4f64c214013a
Brown <Tyler.Brown02@dmschools.org>
2022-02-28 14:22:25 -0600

    final iteration for submission

--- SomeEveryExcersizes.js
+++ SomeEveryExcersizes.js
@@ -41,11 +41,32 @@ function hasOnlyOddNumber(arr){
 // Write a function called hasNoDuplicates which accepts an array and returns true if there are no duplicate values (more than one element in the array that has the same value as another). If there are any duplicates, the function should return false.
 
 function hasNoDuplicates(arr){
-    const tempArr =[];
-    let z = arr.every(function(val){
-        let v = tempArr.indexOf(val) !== -1
-        tempArr.push(val);
-        return v
+    return arr.every(function(val){
+        return arr.indexOf(val) === arr.lastIndexOf(val);
     })
-    return z;
+}
+
+// hasCertainKey
+// Write a function called hasCertainKey which accepts an array of objects and a key, and returns true if every single object in the array contains that key. Otherwise it should return false.
+
+let exaObj = [
+    {title: "Instructor", first: 'Elie', last:"Schoppik"},
+    {title: "Instructor", first: 'Tim', last:"Garcia", isCatOwner: true},
+    {title: "Instructor", first: 'Matt', last:"Lane"},
+    {title: "Instructor", first: 'Colt', last:"Steele", isCatOwner: true}
+  ]
+
+function hasCertainKey(obj, key){
+    return obj.every(function(val){
+        return key in val;
+    })
+}
+
+// hasCertainValue
+// Write a function called hasCertainValue which accepts an array of objects and a key, and a value, and returns true if every single object in the array contains that value for the specific key. Otherwise it should return false.
+
+function hasCertainValue(arr, key, value){
+    return arr.every(function(el){
+        return el[key] === value;
+    })
 }
